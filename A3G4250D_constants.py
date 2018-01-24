8220745254e16170e0d5f4e154874285a3db4160
chisl <info@chisl.io>
2018-01-24 08:59:59 +0100

    Initial commit

---
+++ A3G4250D_constants.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""A3G4250D: MEMS motion sensor: 3-axis digital output gyroscope"""
+
+__author__     = "ChISL"
+__copyright__  = "TBD"
+__credits__    = ["STMicroelectronics"]
+__license__    = "TBD"
+__version__    = "Version 0.1"
+__maintainer__ = "https://chisl.io"
+__email__      = "info@chisl.io"
+__status__     = "Test"
+
+#
+#   THIS FILE IS AUTOMATICALLY CREATED
+#    D O     N O T     M O D I F Y  !
+#
+
+class REG:
+	WHO_AM_I = 15
+	CTRL_REG1 = 32
+	CTRL_REG2 = 33
+	CTRL_REG3 = 34
+	CTRL_REG4 = 35
+	CTRL_REG5 = 36
+	REFERENCE = 37
+	OUT_TEMP = 38
+	STATUS_REG = 39
+	OUT_X_L = 40
+	OUT_Y_L = 42
+	OUT_Z_L = 44
+	FIFO_CTRL_REG = 46
+	FIFO_SRC_REG = 47
+	INT1_CFG = 48
+	INT1_SRC = 49
+	INT1_THS_XH = 50
+	INT1_THS_XL = 51
+	INT1_THS_YH = 52
+	INT1_THS_YL = 53
+	INT1_THS_ZH = 54
+	INT1_THS_ZL = 55
